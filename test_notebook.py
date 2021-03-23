d9e9de28cf0d02e529b61250dd61687f7430403d
ybc <bichengying@gmail.com>
2021-03-22 20:39:58 -0700

    Fix the typo of Sec. 2.7 and update the test by separating ibfrun env

--- test_notebook.py
+++ test_notebook.py
@@ -16,9 +16,10 @@ SKIP_NOTEBOOKS = [
     "Section 7/*.ipynb",
 ]
 
+
 def _list_all_notebooks():
-    output = subprocess.check_output(['git', 'ls-files', '*.ipynb'])
-    return set(output.decode('utf-8').splitlines())
+    output = subprocess.check_output(["git", "ls-files", "*.ipynb"])
+    return set(output.decode("utf-8").splitlines())
 
 
 def _tested_notebooks():
@@ -26,35 +27,49 @@ def _tested_notebooks():
 
     all_notebooks = _list_all_notebooks()
     skipped_notebooks = functools.reduce(
-        lambda a, b: a.union(b), list(
-            set(glob.glob(g, recursive=True)) for g in SKIP_NOTEBOOKS)
+        lambda a, b: a.union(b),
+        list(set(glob.glob(g, recursive=True)) for g in SKIP_NOTEBOOKS),
     )
 
-    return sorted(os.path.abspath(n) for n in all_notebooks.difference(skipped_notebooks))
+    return sorted(
+        os.path.abspath(n) for n in all_notebooks.difference(skipped_notebooks)
+    )
 
-
-@pytest.fixture(scope="session", autouse=True)
-def prepare_ibfrun(request):
-    p_start = subprocess.Popen(
-        'TEST_BLUEFOG_NOTEBOOK=1 ibfrun start -np 4 --disable-heartbeat', shell=True)
-    def _cleanup_func():
-        if not p_start.poll():
-            print("terminate ibfrun start")
-            p_start.terminate()
-        p_stop = subprocess.Popen('ibfrun stop', shell=True)
-        print("run ibfrun stop")
-        p_stop.wait()
-    time.sleep(8)
-    atexit.register(_cleanup_func)
 
 @pytest.mark.parametrize("notebook_path", _tested_notebooks())
 def test_notebooks_against_bluefog(notebook_path):
-    notebook_file = os.path.basename(notebook_path)
-    notebook_rel_dir = os.path.dirname(os.path.relpath(notebook_path, "."))
-    out_path = f".output/{notebook_rel_dir}/{notebook_file[:-6]}.out.ipynb"
-    if not os.path.exists(f".output/{notebook_rel_dir}"):
-        os.makedirs(f".output/{notebook_rel_dir}")
+    p_start = subprocess.Popen(
+        "TEST_BLUEFOG_NOTEBOOK=1 ibfrun start -np 4 --disable-heartbeat", shell=True
+    )
+    time.sleep(8)
 
-    print("Start papermill on ", notebook_path)
-    pm.execute_notebook(notebook_path, out_path, log_output=True, start_timeout=60, execution_timeout=60)
-    print("End papermill")
+    def _cleanup_func():
+        p_stop = subprocess.Popen("ibfrun stop", shell=True)
+        print("run ibfrun stop")
+        p_stop.wait()
+        time.sleep(1)
+        if not p_start.poll():
+            print("terminate ibfrun start")
+            p_start.terminate()
+
+    atexit.register(_cleanup_func)
+    try:
+        notebook_file = os.path.basename(notebook_path)
+        notebook_rel_dir = os.path.dirname(os.path.relpath(notebook_path, "."))
+        out_path = f".output/{notebook_rel_dir}/{notebook_file[:-6]}.out.ipynb"
+        if not os.path.exists(f".output/{notebook_rel_dir}"):
+            os.makedirs(f".output/{notebook_rel_dir}")
+
+        print("Start papermill on ", notebook_path)
+        pm.execute_notebook(
+            notebook_path,
+            out_path,
+            log_output=True,
+            start_timeout=60,
+            execution_timeout=60,
+        )
+        print("End papermill")
+    except:
+        raise RuntimeError(f"Failed to run {notebook_path}")
+    finally:
+        _cleanup_func()
